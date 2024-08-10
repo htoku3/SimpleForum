a21a22d15c5b7fc224ce86c5b79561a4a20f793c
htoku3 <nohohonnVB@hotmail.co.jp>
2024-08-10 18:45:25 +0900

    Initial Commit

---
+++ app.py
@@ -0,0 +1,97 @@
+from datetime import datetime
+from typing import Optional, List
+from flask import Flask
+from flask import render_template, jsonify, request, abort
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=Base)
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://"
+db.init_app(app)
+
+
+class Comment(db.Model):
+    __tablename__ = "comment"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    writer: Mapped[str] = mapped_column(nullable=False)
+    date: Mapped[datetime] = mapped_column(nullable=False)
+    content: Mapped[str] = mapped_column(nullable=False)
+    title: Mapped[str] = mapped_column(nullable=True)
+
+    parent_id: Mapped[int] = mapped_column(db.ForeignKey("comment.id"), nullable=True)
+    parent: Mapped[Optional["Comment"]] = relationship("Comment", remote_side=[id])
+
+    children: Mapped[List["Comment"]] = relationship("Comment", back_populates="parent")
+
+    def to_dict(self):
+        data = dict()
+        data["id"] = self.id
+        data["writer"] = self.writer
+        data["date"] = self.date
+        data["content"] = self.content
+        data["parent_id"] = self.parent_id
+        data["children"] = list(map(lambda child: child.to_dict(), self.children))
+        data["title"] = self.title
+
+        return data
+
+
+@app.route("/")
+def root():
+    return render_template("index.html")
+
+
+@app.route("/forum/get_data", methods=["POST", "GET"])
+def get_forum_data():
+    first_root: Comment = db.session.execute(
+        db.select(Comment).filter_by(parent=None)
+    ).first()
+    return jsonify(first_root.to_dict())
+
+
+@app.route("/forum/append_comment", methods=["POST"])
+def append_comment():
+    new_comment = request.get_json()
+    parent: Optional[Comment]
+    if "parent_id" not in new_comment:
+        parent = None
+    else:
+        parent = db.session.get(Comment, int(new_comment["parent_id"]))
+
+    title = None
+    if parent is None:
+        if "title" in new_comment:
+            title = new_comment["title"]
+        else:
+            return abort(500, "Generating a new topic without title.")
+
+    new_comment = Comment(
+        writer=new_comment["writer"],
+        date=datetime.now(),
+        content=new_comment["content"],
+        parent=parent,
+        title=title,
+    )
+    db.session.add(new_comment)
+
+    if parent:
+        parent.children.append(new_comment)
+    db.session.commit()
+
+    if parent:
+        # Return Parent Tree
+        return jsonify(parent.to_dict())
+    else:
+        # Return generated new root
+        return jsonify(new_comment.to_dict())
+
+
+with app.app_context():
+    db.create_all()
---
+++ static/board.js
@@ -0,0 +1,266 @@
+
+class global_id {
+    constructor() {
+        this.id = 0
+    }
+
+    get() {
+        let id = this.id
+        this.id = this.id + 1
+        return id
+    }
+}
+
+var ID = new global_id()
+
+class Board {
+    static gen_id() {
+        return "id-" + URL.createObjectURL(new Blob()).slice(-36)
+    }
+
+    /**
+     * 
+     * @param {HTMLElement} elem 
+     */
+    static cleanup(elem) {
+        while (elem.firstChild) { elem.removeChild(elem.firstChild) }
+    }
+
+    /**
+     * 
+     * @param {Board} parent 
+     * @param {object} init_data
+     * @param {string} user_name
+     * @param {EventListener} append_comment
+     */
+    constructor(parent, init_data, user_name, append_comment) {
+        this.parent = parent
+        this.data = init_data
+        this.user_name = user_name
+        if (init_data) {
+            this.children = init_data.children.map(data => new Board(this, data, user_name, append_comment))
+        } else {
+            this.children = []
+        }
+        this.append_comment = append_comment
+    }
+
+    get_root() {
+        let board = this
+        while (board.parent) {
+            board = board.parent
+        }
+        return board
+    }
+
+    close_reply_editor() {
+        if (this.parent) {
+            this.editor_div.style.display = "none"
+        }
+        this.card_div.classList.remove("border-primary")
+        this.card_div.classList.add("border-secondary")
+        this.card_div.querySelector(".card-header").classList.remove("text-bg-primary")
+
+        this.children.forEach(element => {
+            element.close_reply_editor()
+        });
+    }
+
+    set_button_status() {
+        this.button.disabled = (this.editor.getText().trim() === "")
+        let title_input = this.card_div.querySelector(".title-input-group")
+        if (title_input.style.display != "none") {
+            this.button.disabled |= title_input.querySelector("input[type='text']").value.trim() === ""
+        }
+    }
+
+    init_container(root_container) {
+        this.div = document.createElement("div")
+        this.div.classList.add("row", "my-2")
+        let div_col = document.createElement("div")
+        div_col.classList.add("col")
+
+        div_col.addEventListener("load", e => {
+
+        })
+        this.card_div = document.createElement("div")
+        this.card_div.tabIndex = ID.get()
+        this.card_div.classList.add("card", "row")
+        this.card_div.addEventListener("focus", e => {
+            this.get_root().close_reply_editor()
+            if (!this.parent) return
+            this.card_div.classList.add("border-primary")
+            this.card_div.querySelector(".card-header").classList.add("text-bg-primary")
+            this.editor_div.style.removeProperty("display")
+        })
+
+        let card_head = document.createElement("div")
+        card_head.classList.add("card-header")
+        this.card_div.appendChild(card_head)
+
+        let card_body = document.createElement("div")
+        card_body.classList.add("card-body")
+
+        let title_input_group = document.createElement("div")
+        title_input_group.classList.add("input-group", "title-input-group")
+        title_input_group.style.display = "none"
+        let title_span = document.createElement("span")
+        title_span.classList.add("input-group-text", "form-control-lg")
+        title_span.textContent = "Title"
+        card_body.appendChild(title_span)
+        let title_input = document.createElement("input")
+        title_input.addEventListener("change", e => this.set_button_status())
+        title_input.classList.add("form-control", "form-control-lg")
+        title_input.type = "text"
+        title_input.id = Board.gen_id()
+        title_input_group.appendChild(title_span)
+        title_input_group.appendChild(title_input)
+        card_body.appendChild(title_input_group)
+
+        let content_div = document.createElement("div")
+        content_div.classList.add("content", "row", "mb-3")
+        content_div.style.display = "none"
+        card_body.appendChild(content_div)
+
+        let hr = document.createElement("div")
+        hr.classList.add("hr", "border-secondary", "border-bottom", "border-3")
+        hr.style.display = "none"
+        card_body.appendChild(hr)
+
+        let children_div = document.createElement("div")
+        children_div.classList.add("children", "row", "border-secondary", "border-3")
+        if (this.parent) {
+            children_div.classList.add("ms-3", "ps-3", "border-start")
+        } else {
+            children_div.classList.add("mx-2", "px-2")
+        }
+        children_div.style.display = "none"
+        card_body.appendChild(children_div)
+
+        this.editor_div = document.createElement("div")
+        this.editor_div.classList.add("row", "my-3")
+        {
+            let editor = document.createElement("div")
+            editor.id = Board.gen_id()
+            this.editor_div.appendChild(editor)
+            this.editor = new Quill(editor, { theme: 'snow' })
+            this.editor.on("text-change", (delta, oldDelta, source) => this.set_button_status())
+
+
+            let btn_comment = document.createElement("button")
+            btn_comment.classList.add("btn", "btn-primary", "my-3")
+            btn_comment.addEventListener("click", e => {
+                let title = null
+                let title_input = this.card_div.querySelector(".title-input-group")
+                if (title_input.style.display != "none") {
+                    title = title_input.querySelector("input[type='text']").value
+                }
+                this.append_comment({
+                    board: this,
+                    comment: {
+                        parent_id: this.data?.id,
+                        writer: this.user_name,
+                        content: this.editor.getSemanticHTML(),
+                        title: title
+                    },
+                })
+                this.editor.deleteText(0, this.editor.getLength())
+            })
+            btn_comment.textContent = this.parent ? "Reply" : "Comment"
+            btn_comment.disabled = true
+            this.button = btn_comment
+            this.editor_div.appendChild(btn_comment)
+        }
+        card_body.appendChild(this.editor_div)
+
+        this.card_div.appendChild(card_body)
+
+        div_col.appendChild(this.card_div)
+        this.div.appendChild(div_col)
+        root_container.appendChild(this.div)
+
+        if (this.data) {
+            this.update_div()
+        } else {
+            this.show_initial()
+        }
+    }
+
+    show_initial() {
+        let card_head = this.card_div.querySelector(".card-header")
+        Board.cleanup(card_head)
+        let writer = document.createElement("span")
+        writer.textContent = this.user_name
+        card_head.appendChild(writer)
+
+        let card_body = this.card_div.querySelector(".card-body")
+        card_body.querySelector(".title-input-group").style.removeProperty("display")
+        card_body.querySelector(".content").style.display = "none"
+        card_body.querySelector(".children").style.display = "none"
+    }
+
+    update_data(data) {
+        this.data = data
+        this.children = data.children.map(child_data => new Board(this, child_data, this.user_name, this.append_comment))
+        this.update_div()
+    }
+
+    update_div() {
+        let card_head = this.card_div.querySelector(".card-header")
+        Board.cleanup(card_head)
+        let writer = document.createElement("span")
+        writer.textContent = this.data.writer
+        let date = document.createElement("small")
+        date.classList.add("mx-3")
+        date.textContent = new Date(this.data.date).toLocaleDateString("ja-JP")
+        card_head.appendChild(writer)
+        card_head.appendChild(date)
+        if (!this.parent && this.data.title) {
+            card_head.appendChild(document.createElement("hr"))
+            let title_span = document.createElement("div")
+            title_span.textContent = this.data.title
+            title_span.classList.add("h3", "me-5")
+            card_head.appendChild(title_span)
+        }
+
+        this.card_div.querySelector(".title-input-group").style.display = "none"
+        let content = this.card_div.querySelector(".content")
+        content.style.removeProperty("display")
+        content.innerHTML = this.data.content
+
+        let children_div = this.card_div.querySelector(".children")
+        Board.cleanup(children_div)
+        if (this.children.length > 0) {
+            this.card_div.querySelector(".hr").style.removeProperty("display")
+            this.children.forEach(board => board.init_container(children_div))
+            children_div.style.removeProperty("display")
+        } else {
+            this.card_div.querySelector(".hr").style.display = "none"
+            children_div.style.display = "none"
+        }
+
+        if (this.parent) {
+            this.editor_div.style.display = "none"
+        } else {
+            this.editor_div.style.removeProperty("display")
+        }
+    }
+}
+
+window.main = function () {
+    var board = new Board(null, null, "foo bar", event => {
+        fetch("/forum/append_comment", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(event.comment)
+        })
+            .then(response => response.json())
+            .then(data => {
+                event.board.update_data(data)
+            })
+    })
+
+    board.init_container(document.getElementById("board"))
+}
